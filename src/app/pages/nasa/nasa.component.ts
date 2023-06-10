8b839d979650bd959992f73d98d0f34eaf1e49ff
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2023-06-10 11:37:45 +0200

    nasa

--- src/app/pages/nasa/nasa.component.ts
+++ src/app/pages/nasa/nasa.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NasaService } from 'src/app/services/nasa/nasa.service';
 
 @Component({
   selector: 'app-nasa',
   templateUrl: './nasa.component.html',
   styleUrls: ['./nasa.component.css']
 })
-export class NasaComponent {
+export class NasaComponent implements OnInit{
 
+  imgOfTheDay!: string;
+
+  constructor(public nasaService: NasaService){}
+
+  ngOnInit(): void {
+    this.nasaService.getImageOfTheDay().subscribe((image: any) => {
+      console.log('retour de api', image)
+      //this.imgOfTheDay = url;
+    });
+  }
 }
--- src/app/services/nasa/nasa.service.ts
+++ src/app/services/nasa/nasa.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NasaService {
 
-  constructor() { }
+  readonly API_URL: string = "https://api.nasa.gov/planetary/apod?api_key=";
+  readonly API_KEY: string = "gNtBPDfrW5LNwxeYqr1n1l7Y49cJgwTtjPCfwwIu";
+
+  constructor(public httpClient: HttpClient) { }
+
+  getImageOfTheDay(): Observable<string>{
+    return this.httpClient.get<string>(this.API_URL + this.API_KEY)
+  }
 }
